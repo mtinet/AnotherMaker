e46e921397b85b2f7c18f78ce1cefeeeb968ee6a
Jason Juhyun Kim <mtinet@hanmail.net>
2021-04-19 14:59:45 +0900

    Update AllFunctions.ino

--- Lilygo-7000g/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
+++ Lilygo-7000g/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
@@ -22,7 +22,7 @@
 #define GSM_PIN ""
 
 // Your GPRS credentials, if any
-const char apn[]  = "h2g2";     //SET TO YOUR APN
+const char apn[]  = "hologram";     //SET TO YOUR APN
 const char gprsUser[] = "";
 const char gprsPass[] = "";
 
